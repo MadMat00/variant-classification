fb2fbe537fb6ff53fc2d0aea40db626874965405
Mattia Rossini <m.rossini@itsrizzoli.it>
2023-10-25 17:05:46 +0200

    check_integrity function and Processor changes

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,24 @@
-def check_integrity(dataframe,log):
+import json
+
+def check_integrity(dataframe, log):
     df = dataframe.copy()
+    
     for col in df.columns:
         assert not df[col].isnull().all()
-    log.write_log("Dataframe integrity check passed", level="SUCCESS")
+
+    column_data = {}
+    for col in df.columns:
+        nan_percentage = df[col].isnull().sum() / len(df)
+        notnan_percentage = 1 - nan_percentage
+        
+        non_nan_values = df[col].dropna().value_counts(normalize=True).to_dict()
+        
+        column_data[col] = {
+            'NaN_percentage': nan_percentage,
+            'notNaN_percentage': notnan_percentage,
+            'unique_values': non_nan_values
+        }
+
+    with open("Data/column_data.json", "w") as f:
+        json.dump(column_data, f, indent=4)
+
