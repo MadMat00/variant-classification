bd399d7b13ee47ebbb4013a69a9b5ffd0adce19d
Matteo Civita <M.civita@itsrizzoli.it>
2023-11-12 14:24:37 +0100

    Versione finale API

--- main.py
+++ main.py
@@ -17,13 +17,10 @@ if __name__ == "__main__":
     log.write_log("Starting program", level="INFO")
     try:
         df = pd.read_csv("data/processed_data.csv")
-        api.get_api_info_from_df(df, path_json="memory2.json")
+        api.get_api_info_from_df(df, path_json="memory.json")
         log.write_log("Program finished successfully", level="SUCCESS")
         df.to_csv("Data/processed_data.csv", index=False)
         log.write_log(f"Total time: {time.time() - start_program_time} seconds", level="DEBUG")
     except KeyboardInterrupt:
         log.write_log("Program interrupted by user", level="CRITICAL")
-        log.write_log(f"Total time: {time.time() - start_program_time} seconds", level="DEBUG")
-    except Exception as e:
-        log.write_log(f"Program finished with error: {e}", level="CRITICAL")
         log.write_log(f"Total time: {time.time() - start_program_time} seconds", level="DEBUG")
--- src/EnsemblAPI.py
+++ src/EnsemblAPI.py
@@ -2,7 +2,7 @@ import httpx, urllib, json, os
 import pandas as pd
 from tqdm import tqdm
 from src.Log import Log
-import time
+import numpy as np
 
 class EnsemblAPI:
     def __init__(self, log:Log) -> None:
@@ -16,33 +16,43 @@ class EnsemblAPI:
             "Origin": "http://asia.ensembl.org",
             "Connection": "keep-alive",
         }
+        
+        # GeneInfo DataFrame
         self.__geneinfo_df = pd.read_csv("data/HCS_region_map.bed", sep="\t", comment="t", header=None)
         header = ["chrom", "chromStart", "chromEnd", "Exon", "GENEINFO"]
         self.__geneinfo_df.columns = header[:len(self.__geneinfo_df.columns)]
+        # Raggruppamento per geneinfo prendendo il minimo dello start e il massimo dello stop
         self.__geneinfo_df = self.__geneinfo_df.groupby("GENEINFO").agg({"chrom": "mean", "chromStart": "min", "chromEnd": "max"})
         self.__geneinfo_df.reset_index(inplace=True)
         
+        # DataFrame dei identificativi dei geni
         self.__transcript_id_df = pd.read_csv("data/GRCh38_genes_MANE_Select.txt", sep="\t", comment="t", header=None)
         header = ["Gene", "ENST", "NM"]
         self.__transcript_id_df.columns = header[:len(self.__geneinfo_df.columns)]
+        
         self.__memory = {}
+        
+        # Chiavi delle api dentro transcript_consequence
+        self.__transcript_consequences_info_column = json.load(open("key.json"))["transcript_consequences"]
+        
+        # Chiavi delle api dentro colocated_variants
+        self.__colocated_variants_info_column = json.load(open("key.json"))["colocated_variants"]
     
-    def __request_data(self, url:str):
+    def __request_data(self, url:str)->dict:
         while True:
             try:
                 r = httpx.get(url, headers=self.__headers)
-                if r.status_code != 200 and r.status_code != 400:
+                if r.status_code != 200 and r.status_code != 400: # Random error
                     self.__log.write_log(f"Could not connect to Ensembl API: {r.status_code}", "ERROR")
                     continue
-                elif r.status_code == 400:
+                elif r.status_code == 400: # Bad Request
                     self.__log.write_log(f"Could not connect to Ensembl API: {r.status_code}", "ERROR")
                     return None
-                break
-            except Exception as e:
+                return r.json()
+            except Exception as e: # Errore nella chiamata
                 self.__log.write_log(f"Could not connect to Ensembl API: {e}", "ERROR")
-        return r.json()
     
-    def __get_variant_type(self, variant:str):
+    def __get_variant_type(self, variant:str)->str:
         """
         Simple heuristics to get the variant type
         
@@ -85,9 +95,11 @@ class EnsemblAPI:
             return "region", new_variant
         
     def __get_transcript_id(self, gene:str):
+        if gene not in self.__transcript_id_df["Gene"]:
+            return None
         transcript_id = self.__transcript_id_df[self.__transcript_id_df["Gene"] == gene]["ENST"].values[0]
         return transcript_id.split(".")[0]
-        
+       
     def __grch37_to_grch38(self, grch37:str, ref:str, alt:str, chrom:str):
         url = f"https://rest.ensembl.org/map/human/GRCh37/{grch37}/GRCh38?"
         r = self.__request_data(url)
@@ -107,54 +119,188 @@ class EnsemblAPI:
                                   ]["GENEINFO"].values[0]
 
     def __get_transcript_consequences_info(self, transcript_consequences:dict):
-        for key in ["sift_score", "polyphen_score"]:
+        api_dict = {}
+        for key in self.__transcript_consequences_info_column:
             try:
-                #TODO: Cercare tutte le informazioni utili
-                ...
+                if "count" in key:
+                    api_dict[key] = len(transcript_consequences[key.replace("_count", "")][0])
+                else:
+                    api_dict[key] = transcript_consequences[key]
             except KeyError:
                 pass
+            
+        return api_dict
 
+    def __get_colocated_variants_info(self, colocated_variants:dict):
+        for dictionary in colocated_variants:
+            for key in self.__colocated_variants_info_column:
+                if key not in dictionary :
+                    break
+                
+                if isinstance(dictionary, dict):
+                    colocated_variants = dictionary
+        
+        api_dict = {}
+        for key in self.__colocated_variants_info_column:
+            try:
+                if "count" in key:
+                    api_dict[key] = len(colocated_variants[key.replace("_count", "")])
+                else:
+                    api_dict[key] = colocated_variants[key]
+            except KeyError:
+                pass
+            except TypeError:
+                if "count" in key:
+                    api_dict[key] = len(colocated_variants[key.replace("_count", "")])
+                else:
+                    api_dict[key] = colocated_variants[0][key]
+            
+        return api_dict
+    
+    def __get_clean_clin_sig_allele(self, clin_sig_allele, alt):
+        weight = {
+            "uncertain_significance": 0,
+            "conflicting_interpretations_of_pathogenicity": 0,
+            "not_provided": 0,
+            "benign/likely_benign": 0.8,
+            "benign": 1,
+            "likely_benign": 0.5,
+            "pathogenic": -1,
+            "likely_pathogenic": -0.5,
+            "pathogenic/likely_pathogenic": -0.8,
+            "other": 0
+        }
+        
+        
+        allele_sig = []
+        value = 0
+        for allele in clin_sig_allele.split(";"):
+            if allele[0] == alt[0]:
+                allele_sig.append(allele)
+        
+        if len(allele_sig) == 1:
+            value = weight[allele_sig[0].split(":")[1]]
+        elif len(allele_sig) == 0:
+            return None
+
+        
+        for allele in allele_sig:
+            value += weight[allele.split(":")[1]]
+            
+        value /= len(allele_sig)
+        
+        if value >= 0.3: return "NEG"
+        elif value <= -0.3: return "POS"
+        else: return "VUS"
+    
     def get_api_info(self, chrom:str, pos:str, ref:str, alt:str, path_json:str):
         first_variant = f"{chrom} {pos} . {ref} {alt} . . ."
+        
+        # Controllo in memoria
         if first_variant in self.__memory:
-            transcript_consequences = self.__memory[first_variant]
-            self.__get_transcript_consequences_info(transcript_consequences)
-            return None
+            api = self.__memory[first_variant]
             
+            try:
+                api_dict = self.__get_transcript_consequences_info(api["transcript_consequences"][0])
+                api_dict = {**self.__get_colocated_variants_info(api["colocated_variants"]), **api_dict}
+            except KeyError:
+                return None
+            
+            # Pulizia del clin_sig_allele
+            api_dict["clin_sig_allele"] = self.__get_clean_clin_sig_allele(api_dict["clin_sig_allele"], alt)
+            
+            for key in ["most_severe_consequence", "variant_class"]:
+                try:
+                    api_dict["variant_class"] = api[key]
+                except KeyError:
+                    pass
+            
+            api_dict["seq_region_name"] = api["seq_region_name"]
+            return api_dict
+        
+        # Valore per la richiesta api di VEP
         result = self.__get_variant_type(first_variant)
         if result is None:
+            self.__log.write_log(f"Risultato del calcolo per la chiamata VEP è {result} quindi non è possibile fare la chiamata", level="ERROR")
             return None
         
+        # Conversione GRCh37 a GRCh38
         grch37 = result[1].split("/")[0]
         variant_type, variant = self.__grch37_to_grch38(grch37, ref, alt, chrom)
 
+        # Ricerca delle GENEINFO
         try:
             geneinfo = self.__get_geneinfo(chrom, pos)
         except IndexError:
             self.__log.write_log(f"Could not find geneinfo for variant {variant}", "ERROR")
             return None
-        transcript_id = self.__get_transcript_id(geneinfo)
         
-        url = f"http://rest.ensembl.org/vep/human/{variant_type}/{variant}?mane=1&transcript_id={transcript_id}&LoF=1&dbNSFP=MutationTaster_pred,LRT_pred,Polyphen2_HDIV_score,1000Gp3_AF&variant_class=1&Geno2MP=1&domains=1&dbscSNV=1&hgvs=1"
+        # Chiamata VEP per informazioni utili
+        transcript_id = self.__get_transcript_id(geneinfo)
+        if transcript_id == None:
+            return None
+        url = f"http://rest.ensembl.org/vep/human/{variant_type}/{variant}?mane=1&transcript_id={transcript_id}&LoF=1&dbNSFP=ALL&variant_class=1&Geno2MP=1&domains=1&dbscSNV=1&hgvs=1"
         r = self.__request_data(url)
         if r == None:
             return None
         
-        transcript_consequences = r[0]["transcript_consequences"][0]
+        # Ricerca dei transcript_consequences e colocated_variants
+        try:
+            transcript_consequences = r[0]["transcript_consequences"][0]
+            colocated_variants = r[0]["colocated_variants"]
+        except KeyError:
+            return None
+         
+        # Salvataggio in memoria
         self.__memory[first_variant] = r[0]
         self.__log.write_log(f"Added variant {first_variant} to memory", "DEBUG")
         with open(path_json, "w") as f:
             json.dump(self.__memory, f, indent=4)
-        self.__get_transcript_consequences_info(transcript_consequences)
+        
+        # Estrazione delle informazioni utili
+        api_dict = self.__get_transcript_consequences_info(transcript_consequences)
+        api_dict = {**self.__get_colocated_variants_info(colocated_variants), **api_dict}
+        
+        # Pulizia del clin_sig_allele
+        api_dict["clin_sig_allele"] = self.__get_clean_clin_sig_allele(api_dict["clin_sig_allele"], alt)
+        
+        # Estrazione del variant_class
+        for key in ["most_severe_consequence", "variant_class"]:
+            try:
+                api_dict["variant_class"] = r[0][key]
+            except KeyError:
+                pass
+        api_dict["seq_region_name"] = r[0]["seq_region_name"]
+        return api_dict
         
     def get_api_info_from_df(self, df:pd.DataFrame, path_json:str):
+        # Creazione delle colonne
+        df[self.__transcript_consequences_info_column] = np.nan
+        df[self.__colocated_variants_info_column] = np.nan
+        
+        # Caricamento della memoria
         if os.path.exists(path_json):
             self.__memory = json.load(open(path_json, "r"))
             self.__log.write_log(f"Loaded {len(self.__memory)} variants from {path_json}", "DEBUG")
     
-        for row in tqdm(df.itertuples(), total=df.shape[0]):
-            chrom = row[1]
-            pos = row[2]
-            ref = row[3]
-            alt = row[4]
-            self.get_api_info(chrom, pos, ref, alt, path_json)
+        for index, row in tqdm(df.iterrows(), total=df.shape[0]):
+            chrom = row["CHROM"]
+            pos = row["POS"]
+            ref = row["REF"]
+            alt = row["ALT"]
+            api_dict = self.get_api_info(chrom, pos, ref, alt, path_json)
+            
+            if api_dict is None:
+                continue
+            
+            # Assegnazione dei valori nelle colonne
+            for key in api_dict:
+                df.at[index, key] = api_dict[key]
+            
+            # Salvataggio dei dati in un CSV
+            if index % 100 == 0 and index > 0:
+                df.head(index).to_csv("data/data_vep.csv", index=False)
+                self.__log.write_log(f"Saved the first {index} in '{path_json}'", level="SUCCESS")
+                
+        df.to_csv("data/data_vep.csv", index=False)
+        return df
